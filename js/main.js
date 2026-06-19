878fd81ba494b8aee3eff57b8b6d1bb95637f5e0
ChestGamers <chestgamers.yt@gmail.com>
2026-06-19 11:13:11 +0200

    main.js

--- js/main.js
+++ js/main.js
@@ -1,33 +1,24 @@
-const tooltip = document.querySelector('.tooltip');
 const keys = document.querySelectorAll('.key');
 const popupBg = document.querySelector('.info__bg');
 const popup = document.querySelector('.info');
+const tooltip = document.querySelector('.tooltip');
 
 keys.forEach(key => {
-	key.addEventListener('click', function() {
-		popup.querySelector('.info__photo').setAttribute('src', this.dataset.photo);
-		popup.querySelector('.info_title').innerText = this.dataset.title;
-		popup.querySelector('.info__text').innerText = this.dataset.description;
-		popupBg.classList.add('active');
-	});
+    key.addEventListener('click', function() {
+        popup.querySelector('.info__photo').src = this.dataset.photo;
+        popup.querySelector('.info_title').innerText = this.dataset.title;
+        popup.querySelector('.info__text').innerText = this.dataset.description;
+        popupBg.classList.add('active');
+    });
 
-	key.addEventListener('mousemove', function(e) {
-		tooltip.innerText = this.dataset.title
-        tooltip.style.top = (e.y + 20) + 'px';
-        tooltip.style.left = (e.x + 20) + 'px';
-	});
+    key.addEventListener('mousemove', function(e) {
+        tooltip.style.display = 'block';
+        tooltip.innerText = this.dataset.title;
+        tooltip.style.left = (e.clientX + 10) + 'px';
+        tooltip.style.top = (e.clientY + 10) + 'px';
+    });
 
-	key.addEventListener('mouseenter', function() {
-		tooltip.style.display = 'block';
-	});
-
-	key.addEventListener('mouseleave', function() {
-		tooltip.style.display = 'none';
-	});
+    key.addEventListener('mouseleave', () => tooltip.style.display = 'none');
 });
 
-document.addEventListener('click', (e) => {
-	if(e.target === popupBg) {
-		popupBg.classList.remove('active');
-	}
-});
+document.querySelector('.info__close').addEventListener('click', () => popupBg.classList.remove('active'));
